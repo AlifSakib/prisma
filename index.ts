3deff56e52c57cdb0255e4cb372d8813da53c21e
alifsakib <alifsakib@gmail.com>
2023-08-11 01:04:52 +0600

    close #4

--- index.ts
+++ index.ts
@@ -3,15 +3,16 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const getAllUsers = await prisma.user.findMany();
-  console.log(getAllUsers);
-  //   const postUser = await prisma.user.create({
-  //     data: {
-  //       email: "alifsakib2.com",
-  //       name: "alif2",
-  //     },
-  //   });
-  //   console.log(postUser);
+  //   const getAllUsers = await prisma.user.findMany();
+  //   console.log(getAllUsers);
+  const postUser = await prisma.user.create({
+    data: {
+      email: "alifsakib.com",
+      name: "alif",
+      age: 34
+    },
+  });
+  console.log(postUser);
 }
 
 main();
